9f8367669a5a441f6683c46b7e41d7bdbb54a70e
michael <michaelxmail@gmail.com>
2017-04-28 21:41:45 -0600

    Updated with install.exe

--- install.bat
+++ install.bat
@@ -1,3 +1,9 @@
 IF NOT EXIST %appdata%\Wireshark\Plugins mkdir %appdata%\Wireshark\Plugins
 copy iec101_dissector.lua %appdata%\Wireshark\Plugins
 copy iec103_dissector.lua %appdata%\Wireshark\Plugins
+copy selcmd_dissector.lua %appdata%\Wireshark\Plugins
+copy extractdata.lua %appdata%\Wireshark\Plugins
+copy extract_dnpdata.lua %appdata%\Wireshark\Plugins
+
+mkdir c:\ser2pcap
+copy Ser2pcap.exe c:\ser2pcap\
